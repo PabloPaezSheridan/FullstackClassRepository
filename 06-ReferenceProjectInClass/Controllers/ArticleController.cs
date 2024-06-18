3fe71a5cb29537f257bef307e62e2532311c4bd0
pablopaezsheridan <pablopaezps97@gmail.com>
2024-06-18 18:34:11 -0300

    Class 10:  Add all the endpoints and some fixings

---
+++ 06-ReferenceProjectInClass/Controllers/ArticleController.cs
@@ -0,0 +1,67 @@
+using Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore;
+
+[ApiController]
+[Route("api/Article")]
+public class ArticleController : ControllerBase
+{
+    private  readonly ArticleRepository _repo;
+    public ArticleController(ArticleRepository repo)
+    {
+        _repo = repo;
+    }
+
+    [HttpGet("{idArticle}")]
+    public ActionResult Get(int idArticle)
+    {
+        var articleToReturn = _repo.Get(idArticle);
+        return Ok(articleToReturn);
+    }
+
+    [HttpGet]
+    public ActionResult Get()
+    {
+        return Ok(_repo.GetAll());
+    }
+
+    [HttpPost]
+    public ActionResult AddArticle([FromBody] ArticleToAdd body)
+    {
+        var newArticle = new Article()
+        {
+            Content = body.Content,
+            Date = DateTime.UtcNow,
+            ImagePath = body.ImagePath,
+            Summary = body.Summary,
+            Title = body.Title
+        }; 
+        return Ok(_repo.AddArticle(newArticle));
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult DeleteArticle(int id)
+    {
+        return Ok(_repo.Delete(id));
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult UpdateArticle(int id, [FromBody] ArticleToUpdate body)
+    {
+        var articleToReturn = DemoArticles.Articles.First(a => a.Id == id);
+        articleToReturn.Content = body.Content;
+        articleToReturn.Summary = body.Summary;
+        articleToReturn.ImagePath = body.ImagePath;
+        _repo.AddArticle(articleToReturn);
+        return Ok();
+    }
+
+    [HttpGet("search")]
+    public ActionResult GetByTitleOrSummary([FromQuery]string searchCriteria)
+    {
+        return Ok(_repo.GetAll().Where(a => a.Summary.Contains(searchCriteria) || a.Title.Contains(searchCriteria)).ToList());
+    }
+
+    
+
+}
